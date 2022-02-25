fdff71444e48a048e84e9b4ff58552aa53185916
igormedina10 <igormedina10@outlook.com>
2022-02-25 19:39:04 -0300

    Exercicios Metodos Java

---
+++ src/Tipodedados/tipodedados.java
@@ -0,0 +1,46 @@
+package Tipodedados;
+
+public class tipodedados {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 100;
+        //short s2 = 400000;
+
+        //int i1 = 100000000;
+        int i2 = 40000;
+
+        long l1 = 100000000000000000L;
+        long l2 = 200400001222455555L;
+
+        //float f1 = 4.5;
+        float f2 =  10.68f;
+
+        double d1 = 85.69;
+        double d2 = 99.04d;
+
+        char c1 = 'w';
+        //char c2 = 'tw';
+        char c3 = '\u0057';
+
+        String st1 ="Fulano";
+        String st2 ="Cicrano";
+        String st3 =" jjs lljd  psk kskols";
+
+       ; String st4 = "24/08/1999";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(true);
+
+
+
+
+
+
+    }
+}
---
+++ src/com/Atividades.java
@@ -0,0 +1,54 @@
+package com;
+
+import javax.print.DocFlavor;
+
+public class Atividades {
+
+    public static void main(String[] args) {
+
+        int i;
+        int I;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //  = 15;
+        int asrn24678nd;
+        int asrn2$4678_nd = 10;
+
+        asrn24678nd = 100;
+        asrn2$4678_nd = 10;
+
+        int quantidadeProduto = 50;
+        //int quantidadeproduto;
+        final int NUMERO_TENTATIVAS = 5;
+        final int numeroTetativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdprod;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(5);
+        System.out.println(asrn24678nd);
+        System.out.println(asrn2$4678_nd);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+    
+
+
+
+    }
+
+
+
+}
